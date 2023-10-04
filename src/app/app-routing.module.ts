dda1282809e6c33fe3f52ac7da002589e05262b1
Gandharthus <72447580+Gandharthus@users.noreply.github.com>
2023-10-04 10:47:16 +0200

    Troubleshooting
    code to troubleshoot
    problem in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SensorListComponent } from './components/sensorList/sensorList.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'sensordata', component : SensorListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { SensorListComponent } from './components/sensorList/sensorList.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensorListComponent
   ],
   imports: [
     BrowserModule,
